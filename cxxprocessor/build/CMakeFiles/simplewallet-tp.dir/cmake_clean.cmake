dac88704c3d96289abc9f7d0bb0a7d9de605a92c
Thong Tran <trungthong184@gmail.com>
2021-07-25 14:12:15 +0700

    modify Dockerfile for pyprocessor; pyclient

---
+++ cxxprocessor/build/CMakeFiles/simplewallet-tp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simplewallet-tp.dir/simplewallet-tp.cpp.o"
+  "bin/simplewallet-tp.pdb"
+  "bin/simplewallet-tp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simplewallet-tp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
